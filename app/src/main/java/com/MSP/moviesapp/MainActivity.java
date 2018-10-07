26ecaad419cce66685838f054e9c25e25150f486
khaled279 <khaled170320@feng.bu.edu.eg>
2018-10-07 03:16:16 +0200

    second commit

--- app/src/main/java/com/MSP/moviesapp/MainActivity.java
+++ app/src/main/java/com/MSP/moviesapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.MSP.moviesapp;
 
+import android.app.usage.UsageEvents;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,44 +15,73 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        String i =null ;
-        String y = null ;
-
-        QueryUtls queryUtls = new QueryUtls() ;
+    QueryUtls queryUtls = new QueryUtls();
+    MovieDetails movieDetails = new MovieDetails("lol" , "lol Again ",0,0) ;
+   String url1 = "https://api.themoviedb.org/3/movie/550?api_key=d41f1774744fae16950ee66bfbd67399\n" ;
 
 
-        final ArrayList<MovieDetails> movies = new ArrayList<>();
-        movies.add(new MovieDetails(queryUtls.name,queryUtls.overView, R.drawable.avengers, R.drawable.avengers_small));
-        movies.add(new MovieDetails("Deadpool 2 (2018)",
-                getString(R.string.deadpool), R.drawable.deadpool, R.drawable.deadpool_small));
-        movies.add(new MovieDetails("The Meg (2018)",
-                getString(R.string.themeg), R.drawable.themeg, R.drawable.themeg_small));
-        movies.add(new MovieDetails("Mission: Impossible - Fallout (2018)",
-                getString(R.string.missionimpossible), R.drawable.missionimpossible, R.drawable.missionimpossible_small));
-        movies.add(new MovieDetails("Black Panther (2018)",
-                getString(R.string.blackpanther), R.drawable.blackpanther, R.drawable.blackpanther_small));
-        movies.add(new MovieDetails("Ant-Man and the Wasp (2018)",
-                getString(R.string.antman), R.drawable.antman, R.drawable.antman_small));
+    private class thisDoesEveryThing extends AsyncTask<String, String, Object> {
 
-        ListView listview = (ListView) findViewById(R.id.listview);
-        listview.setAdapter(new MyAdapter(movies));
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                MovieDetails selectedMovie = movies.get(i);
-                Intent intent = new Intent(getApplicationContext(), MovieDetailsActivity.class);
+        @Override
+        public Object doInBackground(String... strings) {
+            URL url = null;
+            try {
+                url = new URL(url1);
 
-                intent.putExtra("movie", selectedMovie);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+            String jr = "";
+
+            try {
+                jr = queryUtls.makeHttpRequest(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            movieDetails = queryUtls.jsonparser(jr);
+            return movieDetails;
+
+
+        } }
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            thisDoesEveryThing nice = new thisDoesEveryThing() ;
+            nice.execute();
+
+            final ArrayList<MovieDetails> movies = new ArrayList<>();
+            movies.add(new MovieDetails(movieDetails.getName(), movieDetails.getDes(), R.drawable.avengers, R.drawable.avengers_small));
+            movies.add(new MovieDetails("Deadpool 2 (2018)",
+                    getString(R.string.deadpool), R.drawable.deadpool, R.drawable.deadpool_small));
+            movies.add(new MovieDetails("The Meg (2018)",
+                    getString(R.string.themeg), R.drawable.themeg, R.drawable.themeg_small));
+            movies.add(new MovieDetails("Mission: Impossible - Fallout (2018)",
+                    getString(R.string.missionimpossible), R.drawable.missionimpossible, R.drawable.missionimpossible_small));
+            movies.add(new MovieDetails("Black Panther (2018)",
+                    getString(R.string.blackpanther), R.drawable.blackpanther, R.drawable.blackpanther_small));
+            movies.add(new MovieDetails("Ant-Man and the Wasp (2018)",
+                    getString(R.string.antman), R.drawable.antman, R.drawable.antman_small));
+
+            ListView listview = (ListView) findViewById(R.id.listview);
+            listview.setAdapter(new MyAdapter(movies));
+            listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    MovieDetails selectedMovie = movies.get(i);
+                    Intent intent = new Intent(getApplicationContext(), MovieDetailsActivity.class);
+
+                    intent.putExtra("movie", selectedMovie);
                 /*
                 Bundle bundle = new Bundle();
                 bundle.putString("name", selectedMovie.getName());
@@ -58,47 +90,49 @@ public class MainActivity extends AppCompatActivity {
 
                 intent.putExtras(bundle);
                 */
-                startActivity(intent);
+                    startActivity(intent);
+                }
+            });
+        }
+
+
+        public class MyAdapter extends BaseAdapter {
+
+            private ArrayList<MovieDetails> movies;
+
+            public MyAdapter(ArrayList<MovieDetails> movies) {
+                this.movies = movies;
             }
-        });
-    }
 
-    public class MyAdapter extends BaseAdapter{
+            @Override
+            public int getCount() {
+                return movies.size();
+            }
 
-        private ArrayList<MovieDetails> movies;
+            @Override
+            public MovieDetails getItem(int i) {
+                return movies.get(i);
+            }
 
-        public MyAdapter(ArrayList<MovieDetails> movies) {
-            this.movies = movies;
+            @Override
+            public long getItemId(int i) {
+                return i;
+            }
+
+            @Override
+            public View getView(int i, View view, ViewGroup viewGroup) {
+                LayoutInflater inflater = getLayoutInflater();
+                view = inflater.inflate(R.layout.list_item, null);
+
+                ImageView img = view.findViewById(R.id.img);
+                TextView txtName = view.findViewById(R.id.txtname);
+
+                MovieDetails currentMovie = getItem(i);
+                img.setImageResource(currentMovie.getsImgId());
+                txtName.setText(currentMovie.getName());
+
+                return view;
+            }
         }
 
-        @Override
-        public int getCount() {
-            return movies.size();
-        }
-
-        @Override
-        public MovieDetails getItem(int i) {
-            return movies.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
-            LayoutInflater inflater = getLayoutInflater();
-            view = inflater.inflate(R.layout.list_item, null);
-
-            ImageView img = view.findViewById(R.id.img);
-            TextView txtName = view.findViewById(R.id.txtname);
-
-            MovieDetails currentMovie = getItem(i);
-            img.setImageResource(currentMovie.getsImgId());
-            txtName.setText(currentMovie.getName());
-
-            return view;
-        }
-    }
 }
--- app/src/main/java/com/MSP/moviesapp/QueryUtls.java
+++ app/src/main/java/com/MSP/moviesapp/QueryUtls.java
@@ -1,14 +1,17 @@
 package com.MSP.moviesapp;
 
 import android.app.Activity;
+import android.app.usage.UsageEvents;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.EventLog;
 import android.util.JsonReader;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -17,71 +20,93 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import javax.net.ssl.HttpsURLConnection;
-
-public class QueryUtls {
-    String  name;
-    String overView ;
-    String keepImageUrl  ;
-    String addToImageUrl = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" ;
-    String url1 ;
-
-
-
-    protected void getData() {
-
-       url1= "https://api.themoviedb.org/3/movie/550?api_key=d41f1774744fae16950ee66bfbd67399\n";
-        URL url = null;
-        JsonReader json1 ;
-        try {
-            url = new URL(url1);
-
-            HttpsURLConnection link = (HttpsURLConnection) url.openConnection();
-
-            InputStream input = link.getInputStream();
-            InputStreamReader inputReader = new InputStreamReader(input, "utf-8");
-
-            String getData = inputReader.toString();
+import android.os.AsyncTask;
+public  class QueryUtls {
+        String  name;
+        String overView ;
+        String keepImageUrl  ;
+        String addToImageUrl = "https://image.tmdb.org/t/p/w600_and_h900_bestv2/" ;
+        String url1 ="https://api.themoviedb.org/3/movie/550?api_key=d41f1774744fae16950ee66bfbd67399\n" ;
+        String jsonResponse = "" ;
+        InputStream is = null ;
+        HttpsURLConnection urlConnection = null ;
+        MovieDetails movieDetails ;
 
 
+    public  String makeHttpRequest (URL url1) throws IOException {
 
-            JSONObject root = new JSONObject(getData);
+       try {
+           urlConnection = (HttpsURLConnection) url1.openConnection();
+           urlConnection.setRequestMethod("GET");
+           urlConnection.setReadTimeout(10000);
+           urlConnection.setConnectTimeout(15000);
+           urlConnection.connect();
+           is = urlConnection.getInputStream();
+           jsonResponse = readFromStream(is) ;
 
-             name = root.getString("original_title");
-             overView = root.getString("overview");
-
-//second way of parsing (both not working)
-          /*  json1 = new JsonReader(inputReader);
-            json1.beginObject();
-            while (json1.hasNext()) {
-                String key = json1.nextName();
-                if (key.equals("original_title")) {
-                    name = json1.nextString();
-                }
-                if(key.equals("overview")){
-
-                    overView = json1.nextString() ;
-
-                }
-                if(key.equals("poster_path")){
-
-                    keepImageUrl=addToImageUrl+json1.nextString() ;
-                }
+       }
+       catch (IOException e ){}
 
 
-                else {
-                    json1.skipValue();
-                }}*/
+       finally {
+           if (urlConnection != null) {
+               urlConnection.disconnect();
+           }
+           if (is != null) {
+               // function must handle java.io.IOException here
+               is.close();
+           }
 
 
 
-} catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
+    return jsonResponse ;  }
+
+
+}
+    private String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            String line = reader.readLine();
+            while (line != null) {
+                output.append(line);
+                line = reader.readLine();
+            }
         }
+        return output.toString();
     }
+
+    public MovieDetails jsonparser(String jsonfile){
+        try {
+            JSONObject json = new JSONObject(jsonfile) ;
+
+            if(json != null){
+
+                name = json.getString("original_title");
+                overView = json.getString("overview");
+                return new MovieDetails(name,overView,0,0) ;
+            }
+
+
+
+        }
+
+
+        catch (JSONException e){}
+
+
+
+
+
+
+
+    return null ;}
+
+
+
+
+
 
 
 }
